d3d8e44e420dcef34dcc0398c08ef492516c2984
sanjana <sanjanacvks@gmail.com>
2024-03-28 10:45:37 +0530

    response entity is added for http requests

--- src/main/java/com/example/QuizApplication/controller/QuestionController.java
+++ src/main/java/com/example/QuizApplication/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.example.QuizApplication.controller;
 import com.example.QuizApplication.model.Question;
 import com.example.QuizApplication.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,15 +14,15 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 
--- src/main/java/com/example/QuizApplication/service/QuestionService.java
+++ src/main/java/com/example/QuizApplication/service/QuestionService.java
@@ -3,24 +3,48 @@ package com.example.QuizApplication.service;
 import com.example.QuizApplication.dao.QuestionDao;
 import com.example.QuizApplication.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+
+    //Getting Questions from the database
+    public ResponseEntity<List<Question>> getAllQuestions() {
+
+     try {
+         return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+         }
+     catch (Exception e) {
+         e.printStackTrace();
+     }
+     return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+
+    //Getting questions by category
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
+
+    //Adding Question to the database
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
     }
 }
